589ce782a0f6f3ea62e6eac2e1677ac009adfd2a
Julie McDonald <56400444+julielmcdonald@users.noreply.github.com>
2025-04-27 08:04:51 -0400

    Update sorted_bam_to_master.sh

--- sorted_bam_to_master.sh
+++ sorted_bam_to_master.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 #SBATCH -N 1
 #SBATCH -n 4
-#SBATCH --mail-type=END
-#SBATCH --mail-user=jlmcd@mit.edu
 ###################
 
 module add samtools/1.3
@@ -17,9 +15,9 @@ cd $target_directory
 echo samtools mpileup -d 10000000 -excl-reads 2052 -f $reference_genome -b $sample_list > $experiment_name'_mpileup.txt'
 samtools mpileup -d 10000000 -excl-reads 2052 -f $reference_genome -b $sample_list > $experiment_name'_mpileup.txt'
 
-echo cat $sample_list | perl /net/bmc-pub14/data/shoulders/public/Jess/VirCat/master_table_header_generator_with_indels.pl > $experiment_name'_master_table.txt'
-cat $sample_list | perl /net/bmc-pub14/data/shoulders/public/Jess/VirCat/master_table_header_generator_with_indels.pl > $experiment_name'_master_table.txt' # might require some tweaking to go from the file names
-#in the sample_list file to simpler names to be used in the master table header
+echo cat $sample_list | perl master_table_header_generator_with_indels.pl > $experiment_name'_master_table.txt'
+cat $sample_list | perl master_table_header_generator_with_indels.pl > $experiment_name'_master_table.txt'
 
-echo cat $experiment_name'_mpileup.txt' | perl /net/bmc-pub14/data/shoulders/public/Jess/VirCat/master_table_generator_indel_counter.pl >>  $experiment_name'_master_table.txt'
-cat $experiment_name'_mpileup.txt' | perl /net/bmc-pub14/data/shoulders/public/Jess/VirCat/master_table_generator_indel_counter.pl >>  $experiment_name'_master_table.txt'
+
+echo cat $experiment_name'_mpileup.txt' | perl master_table_generator_indel_counter.pl >>  $experiment_name'_master_table.txt'
+cat $experiment_name'_mpileup.txt' | perl master_table_generator_indel_counter.pl >>  $experiment_name'_master_table.txt'
