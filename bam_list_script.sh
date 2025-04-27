7c76d221676359423a558a60b5f48e29e253e394
Julie McDonald <56400444+julielmcdonald@users.noreply.github.com>
2025-04-27 08:02:08 -0400

    Update bam_list_script.sh

--- bam_list_script.sh
+++ bam_list_script.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 #SBATCH -N 1
 #SBATCH -n 4
-#SBATCH --nice=100000
-#SBATCH --exclude=c[5-22]
-#SBATCH --mail-type=END
-#SBATCH --mail-user=jlmcd@mit.edu
 ###################
 
 sample_no_zeros=$1
 out=$2
 
-ls 240408Shodata_"$sample_no_zeros"__out.dn10pct.bam > /net/bmc-pub14/data/shoulders/users/jlmcd/240408Sho/"$out"_bam_list.txt
+ls "$sample_no_zeros"__out.dn10pct.bam > "$out"_bam_list.txt
 
 
